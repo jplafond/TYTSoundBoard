fca93316efe7074958136203edfe87dccddf32bc
Jp <jp.lafond@gmail.com>
2017-08-09 18:35:57 -0400

    Added SoundsTableViewController editing:   * insert/remove/rearrange   * refresh

--- TYTSoundboard/controllers/SoundsTableViewController.swift
+++ TYTSoundboard/controllers/SoundsTableViewController.swift
@@ -10,16 +10,23 @@ import UIKit
 
 class SoundsTableViewController: UITableViewController {
 
-    var allSounds = Sound.allClips()
+    var allSounds: [Sound]?
+
+    var addButtonItem: UIBarButtonItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
+        addButtonItem = UIBarButtonItem(barButtonSystemItem: .add,
+                                        target: self,
+                                        action: #selector(addButtonPressed(sender:)))
 
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        self.clearsSelectionOnViewWillAppear = false
+
+        self.navigationItem.leftBarButtonItem = self.editButtonItem
+        self.navigationItem.rightBarButtonItem = self.addButtonItem
+
+        populateModel()
     }
 
     override func didReceiveMemoryWarning() {
@@ -51,40 +58,37 @@ class SoundsTableViewController: UITableViewController {
         return cell
     }
 
-    /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            allSounds?.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath],
+                                 with: .fade)
+        }
     }
-    */
 
-    /*
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
 
-    }
-    */
+        guard var allSounds = allSounds else {
+            return
+        }
 
-    /*
+        let sound = allSounds.remove(at: fromIndexPath.row)
+        allSounds.insert(sound, at: to.row)
+    }
+
     // Override to support conditional rearranging of the table view.
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the item to be re-orderable.
         return true
     }
-    */
 
     /*
     // MARK: - Navigation
@@ -96,6 +100,29 @@ class SoundsTableViewController: UITableViewController {
     }
     */
 
+}
+
+// MARK: - Table IBActions
+extension SoundsTableViewController {
+    @IBAction func refreshTriggered(sender: UIRefreshControl) {
+        populateModel()
+
+        sender.endRefreshing()
+    }
+
+    @IBAction func addButtonPressed(sender: UIBarButtonItem) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        let sound = Sound(name: "*New* Animals R Innocent")
+        let indexPath = IndexPath(row: 0, section: 0)
+        allSounds?.insert(sound, at: indexPath.row)
+        tableView.insertRows(at: [indexPath],
+                             with: .automatic)
+    }
+
+    func populateModel() {
+        allSounds = Sound.allClips()
+        tableView.reloadData()
+    }
 }
 
 // MARK: - Table Specific Constants
